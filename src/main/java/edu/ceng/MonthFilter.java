9f8891d63f97119dab03d6562edbd3b7ba189eff
esrakoca <esrakoca803@anadolu.edu.tr>
2016-12-30 12:18:49 +0300

    anything

---
+++ src/main/java/edu/ceng/MonthFilter.java
@@ -0,0 +1,19 @@
+package edu.ceng;
+
+import java.time.Month;
+import java.util.function.Predicate;
+
+public class MonthFilter implements Predicate<Article> {
+
+    private final Month month;
+
+    public MonthFilter(Month month){
+        this.month = month;
+    }
+
+    @Override
+    public boolean test(Article article) {
+        return article.getMonth().equals(month);
+    }
+
+}
